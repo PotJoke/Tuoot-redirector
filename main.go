606ff25f99342496585cea3be5986881ca088b10
PotJoke <bulankov69@gmail.com>
2026-02-22 23:51:12 +0300

    GET redirection!

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 	http.HandleFunc("/test", PostGetTest)
 
 	log.Fatal(http.ListenAndServe(port, nil))
+
+	fmt.Print(http.Get("http://localhost" + port + "/test"))
 }
 
 func PostGetTest(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +26,14 @@ func PostGetTest(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		io.WriteString(w, "POST METHOD")
 	case "GET":
-		io.WriteString(w, "GET METHOD")
+		if r.URL.RawQuery == "" {
+			io.WriteString(w, "GET METHOD")
+			return
+		}
+		res, _ := http.Get(r.URL.RawQuery)
+		body, _ := io.ReadAll(res.Body)
+		io.WriteString(w, string(body))
+
 	default:
 		io.WriteString(w, "METHOD NOT ALLOWED")
 	}
